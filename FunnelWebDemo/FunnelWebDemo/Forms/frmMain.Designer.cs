d0ab5f8fd02544da6933031efa664602246d0da1
RealMiSeRey <55806260+RealMiSeRey@users.noreply.github.com>
2019-09-29 13:48:33 +0200

    Add files via upload

---
+++ FunnelWebDemo/FunnelWebDemo/Forms/frmMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace FunnelWebDemo
+{
+    partial class frmMain2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain2));
+            this.splitContainerControlMain = new DevExpress.XtraEditors.SplitContainerControl();
+            this.treSelection = new DevExpress.XtraTreeList.TreeList();
+            this.colcrlID = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            this.colcatIDParent = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            this.colcatID = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            this.colcatDescription = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            this.colurlURL = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            this.vwCategoriesSecondBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.funnelWeb_DemoDataSet3 = new FunnelWebDemo.FunnelWeb_DemoDataSet3();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.vwCategoriesSecondTableAdapter = new FunnelWebDemo.FunnelWeb_DemoDataSet3TableAdapters.vwCategoriesSecondTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControlMain)).BeginInit();
+            this.splitContainerControlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.treSelection)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vwCategoriesSecondBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.funnelWeb_DemoDataSet3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainerControlMain
+            // 
+            this.splitContainerControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControlMain.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControlMain.Name = "splitContainerControlMain";
+            this.splitContainerControlMain.Panel1.Controls.Add(this.treSelection);
+            this.splitContainerControlMain.Panel1.Text = "Panel1";
+            this.splitContainerControlMain.Panel2.Controls.Add(this.webBrowser);
+            this.splitContainerControlMain.Panel2.Text = "Panel2";
+            this.splitContainerControlMain.Size = new System.Drawing.Size(1681, 1072);
+            this.splitContainerControlMain.SplitterPosition = 493;
+            this.splitContainerControlMain.TabIndex = 0;
+            // 
+            // treSelection
+            // 
+            this.treSelection.Columns.AddRange(new DevExpress.XtraTreeList.Columns.TreeListColumn[] {
+            this.colcrlID,
+            this.colcatIDParent,
+            this.colcatID,
+            this.colcatDescription,
+            this.colurlURL});
+            this.treSelection.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treSelection.DataSource = this.vwCategoriesSecondBindingSource;
+            this.treSelection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treSelection.Location = new System.Drawing.Point(0, 0);
+            this.treSelection.Name = "treSelection";
+            this.treSelection.Size = new System.Drawing.Size(493, 1072);
+            this.treSelection.TabIndex = 0;
+            this.treSelection.FocusedNodeChanged += new DevExpress.XtraTreeList.FocusedNodeChangedEventHandler(this.treSelection_FocusedNodeChanged);
+            // 
+            // colcrlID
+            // 
+            this.colcrlID.FieldName = "crlID";
+            this.colcrlID.Name = "colcrlID";
+            this.colcrlID.Visible = true;
+            this.colcrlID.VisibleIndex = 0;
+            // 
+            // colcatIDParent
+            // 
+            this.colcatIDParent.FieldName = "catIDParent";
+            this.colcatIDParent.Name = "colcatIDParent";
+            this.colcatIDParent.Visible = true;
+            this.colcatIDParent.VisibleIndex = 1;
+            // 
+            // colcatID
+            // 
+            this.colcatID.FieldName = "catID";
+            this.colcatID.Name = "colcatID";
+            this.colcatID.Visible = true;
+            this.colcatID.VisibleIndex = 2;
+            // 
+            // colcatDescription
+            // 
+            this.colcatDescription.FieldName = "catDescription";
+            this.colcatDescription.Name = "colcatDescription";
+            this.colcatDescription.Visible = true;
+            this.colcatDescription.VisibleIndex = 3;
+            // 
+            // colurlURL
+            // 
+            this.colurlURL.FieldName = "urlURL";
+            this.colurlURL.Name = "colurlURL";
+            this.colurlURL.Visible = true;
+            this.colurlURL.VisibleIndex = 4;
+            // 
+            // vwCategoriesSecondBindingSource
+            // 
+            this.vwCategoriesSecondBindingSource.DataMember = "vwCategoriesSecond";
+            this.vwCategoriesSecondBindingSource.DataSource = this.funnelWeb_DemoDataSet3;
+            // 
+            // funnelWeb_DemoDataSet3
+            // 
+            this.funnelWeb_DemoDataSet3.DataSetName = "FunnelWeb_DemoDataSet3";
+            this.funnelWeb_DemoDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1178, 1072);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // vwCategoriesSecondTableAdapter
+            // 
+            this.vwCategoriesSecondTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmMain2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1681, 1072);
+            this.Controls.Add(this.splitContainerControlMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain2";
+            this.Text = "FunnelWeb Demo 2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmMain2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControlMain)).EndInit();
+            this.splitContainerControlMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.treSelection)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vwCategoriesSecondBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.funnelWeb_DemoDataSet3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControlMain;
+        private DevExpress.XtraTreeList.TreeList treSelection;
+        private FunnelWeb_DemoDataSet3 funnelWeb_DemoDataSet3;
+        private System.Windows.Forms.BindingSource vwCategoriesSecondBindingSource;
+        private FunnelWeb_DemoDataSet3TableAdapters.vwCategoriesSecondTableAdapter vwCategoriesSecondTableAdapter;
+        private DevExpress.XtraTreeList.Columns.TreeListColumn colcrlID;
+        private DevExpress.XtraTreeList.Columns.TreeListColumn colcatIDParent;
+        private DevExpress.XtraTreeList.Columns.TreeListColumn colcatID;
+        private DevExpress.XtraTreeList.Columns.TreeListColumn colcatDescription;
+        private DevExpress.XtraTreeList.Columns.TreeListColumn colurlURL;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
---
+++ FunnelWebDemo/FunnelWebDemo/Forms/frmMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetBrowser;
+using DotNetBrowser.WinForms;
+using System.Net;
+
+namespace FunnelWebDemo
+{
+    public partial class frmMain2 : Form
+    {
+        public frmMain2()
+        {
+            InitializeComponent();
+            treSelection.KeyFieldName = "catID";
+            treSelection.ParentFieldName = "catIDParent";
+        }
+
+        private void frmMain2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'funnelWeb_DemoDataSet3.vwCategoriesSecond' table. You can move, or remove it, as needed.
+            this.vwCategoriesSecondTableAdapter.Fill(this.funnelWeb_DemoDataSet3.vwCategoriesSecond);
+            treSelection.Columns.ColumnByName("colcrlID").Visible = false;
+            treSelection.Columns.ColumnByName("colurlURL").Visible = false;
+            treSelection.ExpandAll();
+        }
+
+        private void treSelection_FocusedNodeChanged(object sender, DevExpress.XtraTreeList.FocusedNodeChangedEventArgs e)
+        {
+            string thisURL = Convert.ToString(treSelection.FocusedNode.GetValue(2));
+            if (thisURL.StartsWith("http"))
+            {
+                var response = new WebClient().DownloadString(thisURL);
+                webBrowser.Navigate(thisURL);
+            }
+        }
+        }
+}
