4848ab818baa34708f4b5fcd52b07f0b0f1d3622
RealMiSeRey <55806260+RealMiSeRey@users.noreply.github.com>
2019-09-29 13:53:22 +0200

    Add files via upload

---
+++ FunnelWebDemo/FunnelWebDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunnelWebDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (!FunnelWebDemo.Classes.WBEmulator.IsBrowserEmulationSet())
+            {
+                FunnelWebDemo.Classes.WBEmulator.SetBrowserEmulationVersion();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(true);
+            Application.Run(new frmMain2());
+        }
+    }
+}
